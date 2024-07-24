c5b419f956fda510b12f9c0db4a51213eb84cf9d
GustavoHenriquePSilva <gustavorj088@gmail.com>
2024-07-23 23:28:59 -0300

    Commit lanchonete

---
+++ exemplo-pacotes/src/lanchonete/atendimento/cozinha/Almoxarife.java
@@ -0,0 +1,17 @@
+package lanchonete.atendimento.cozinha;
+
+public class Almoxarife {
+    private void controlarEntrada(){
+        System.out.println("CONTROLANDO A ENTRADA DOS ITENS");
+    }
+    private void controlarSaida(){
+        System.out.println("CONTROLANDO A SAIDA DOS ITENS");
+    }
+    void entregarIgredientes(){
+        System.out.println("ENTREGANDO IGREDIENTES");
+        controlarSaida();
+    }
+    void trocarGas(){
+        System.out.println("ALMOXARIFE TROCANDO O GAS");
+    }
+}
